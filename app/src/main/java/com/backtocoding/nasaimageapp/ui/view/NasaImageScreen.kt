fc70e0f3fbc832bb8908fde49c0d42c10e721db9
MilindAmrutkar <milind27.amrutkar@gmail.com>
2023-09-01 18:04:33 +0530

    Added: ui improvements

--- app/src/main/java/com/backtocoding/nasaimageapp/ui/view/NasaImageScreen.kt
+++ app/src/main/java/com/backtocoding/nasaimageapp/ui/view/NasaImageScreen.kt
@@ -1,9 +1,12 @@
 package com.backtocoding.nasaimageapp.ui.view
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,9 +14,16 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -64,19 +74,47 @@ fun NasaImageContent(
             .padding(16.dp)
     ) {
         item {
-            Text(text = title)
+            Text(
+                text = title,
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 24.sp,
+                    letterSpacing = 0.15.sp
+                )
+            )
         }
-        item { Text(text = date) }
+        item {
+            Text(
+                text = date,
+                style = TextStyle(
+                    fontWeight = FontWeight.Light,
+                    fontSize = 14.sp,
+                    color = Color.DarkGray
+                )
+            )
+        }
         item {
             AsyncImage(
                 model = ImageRequest.Builder(LocalContext.current)
                     .data(imageUrl).build(),
                 contentDescription = "Nasa Image",
                 placeholder = painterResource(id = R.drawable.ic_error),
-                error = painterResource(id = R.drawable.ic_error)
+                error = painterResource(id = R.drawable.ic_error),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(16 / 9f)
+                    .clip(RoundedCornerShape(4.dp))
             )
         }
-        item { Text(text = description) }
+        item {
+            Text(
+                text = description,
+                style = TextStyle(fontWeight = FontWeight.Normal, fontSize = 16.sp),
+                textAlign = TextAlign.Justify,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
     }
 
 }
